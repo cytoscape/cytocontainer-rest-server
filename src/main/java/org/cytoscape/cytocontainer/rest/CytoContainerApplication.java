62b112b1bc7bb5d0bc55516671748dab972398bc
Chris Churas <churas.camera@gmail.com>
2024-09-27 11:34:02 -0700

    added openapi classes and StringMessageBodyWriter class

--- src/main/java/org/cytoscape/cytocontainer/rest/CytoContainerApplication.java
+++ src/main/java/org/cytoscape/cytocontainer/rest/CytoContainerApplication.java
@@ -1,13 +1,8 @@
 package org.cytoscape.cytocontainer.rest; 
 
 
-import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
-import io.swagger.v3.oas.integration.OpenApiConfigurationException;
-import io.swagger.v3.oas.integration.SwaggerConfiguration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import jakarta.servlet.ServletConfig;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Context;
@@ -15,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.cytoscape.cytocontainer.rest.services.CustomOpenApiResource;
+import org.cytoscape.cytocontainer.rest.engine.util.StringMessageBodyWriter;
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import org.cytoscape.cytocontainer.rest.services.CytoContainer;
 import org.cytoscape.cytocontainer.rest.services.Status;
@@ -34,35 +29,8 @@ public class CytoContainerApplication extends Application {
         corsFilter.setAllowCredentials(true);
         _singletons.add(corsFilter);
 		
-		OpenAPI oas = new OpenAPI();
-        Info info = new Info()
-                .title("Swagger Sample App bootstrap code")
-                .description("This is a sample server Petstore server.  You can find out more about Swagger " +
-                        "at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, " +
-                        "you can use the api key `special-key` to test the authorization filters.")
-                .termsOfService("http://swagger.io/terms/")
-                .contact(new Contact()
-                        .email("apiteam@swagger.io"))
-                .license(new License()
-                        .name("Apache 2.0")
-                        .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
-
-        oas.info(info);
-        SwaggerConfiguration oasConfig = new SwaggerConfiguration()
-                .openAPI(oas)
-                .prettyPrint(true)
-                .resourcePackages(Stream.of("io.swagger.sample.resource").collect(Collectors.toSet()));
-
-        try {
-            new JaxrsOpenApiContextBuilder()
-                    .servletConfig(servletConfig)
-                    .application(this)
-                    .openApiConfiguration(oasConfig)
-                    .buildContext(true);
-        } catch (OpenApiConfigurationException e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-		_logger.info("Did something here, not sure what");
+		
+		_logger.info("Initializing Application");
     }
     @Override
     public Set<Object> getSingletons() {
@@ -74,7 +42,9 @@ public class CytoContainerApplication extends Application {
         // @TODO add logic to ony add Diffusion.class if there is a diffusion
         //       algorithm present
         return Stream.of(CytoContainer.class,
-				         CustomOpenApiResource.class,
+				         OpenApiResource.class, 
+						 AcceptHeaderOpenApiResource.class,
+						 StringMessageBodyWriter.class,
                          Status.class).collect(Collectors.toSet());
     }
 }
